fc89045ecf5ce39292f6d4cccceb1958c9b2d5ad
Alberto Soler <albertosolpal@gmail.com>
2023-05-02 09:05:11 +0200

    test: admin proyect

--- apps/caskchain-admin/pages/_app.tsx
+++ apps/caskchain-admin/pages/_app.tsx
@@ -1,34 +1,35 @@
 import '../scripts/wdyr'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Web3Provider } from '@providers'
+// import type { AppProps } from 'next/app'
+// import { Web3Provider } from '@providers'
 
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import GlobalProvider from '@providers/global'
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { localhost, polygonMumbai } from 'wagmi/chains'
-import { AnimatePresence } from 'framer-motion'
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+// import GlobalProvider from '@providers/global'
+// import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+// import { Web3Modal } from '@web3modal/react'
+// import { configureChains, createClient, WagmiConfig } from 'wagmi'
+// import { localhost, polygonMumbai } from 'wagmi/chains'
+// import { AnimatePresence } from 'framer-motion'
 
-const chains = [localhost, polygonMumbai]
-const projectId = '7ba8a5909e41332fb0abe840c1d4923e'
+// const chains = [localhost, polygonMumbai]
+// const projectId = '7ba8a5909e41332fb0abe840c1d4923e'
 
-const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+// const { provider } = configureChains(chains, [w3mProvider({ projectId })])
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider,
-})
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ projectId, version: 1, chains }),
+//   provider,
+// })
 
-const ethereumClient = new EthereumClient(wagmiClient, chains)
+// const ethereumClient = new EthereumClient(wagmiClient, chains)
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App() {
   return (
     <>
-      <ToastContainer theme="dark" closeOnClick />
+      <h1>HOLA</h1>
+      {/* <ToastContainer theme="dark" closeOnClick />
       <GlobalProvider>
         <WagmiConfig client={wagmiClient}>
           <Web3Provider>
@@ -38,7 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
           </Web3Provider>
         </WagmiConfig>
-      </GlobalProvider>
+      </GlobalProvider> */}
     </>
   )
 }
