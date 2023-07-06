967072f293df367ed40f04c00fe30ebbe8002e6f
Alberto Soler <albertosolpal@gmail.com>
2023-07-06 10:45:55 +0200

    feat: removed image from cask

--- apps/caskchain-admin/pages/casks/[caskId].tsx
+++ apps/caskchain-admin/pages/casks/[caskId].tsx
@@ -2,7 +2,7 @@ import { useCaskNft } from '@hooks/web3'
 import { BaseLayout } from '@ui'
 import { Button, Spacer } from 'caskchain-ui'
 import { NextPageContext } from 'next'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { ethers } from 'ethers'
 
